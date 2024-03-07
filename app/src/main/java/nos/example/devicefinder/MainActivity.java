ad731d65b1cdf8664f3b33e2db9c42d6012d1a5b
savesantos <salvadorvelososantos@gmail.com>
2024-03-07 15:57:17 +0000

    better vibration

--- app/src/main/java/nos/example/devicefinder/MainActivity.java
+++ app/src/main/java/nos/example/devicefinder/MainActivity.java
@@ -132,20 +132,17 @@ public class MainActivity extends AppCompatActivity {
                     });
 
                     int vibrationAmplitude = calculateVibrationAmplitude(Math.abs(wifiStateExtra));
-                    long vibrationDuration = calculateVibrationDuration(Math.abs(wifiStateExtra));
+                    int vibrationDuration = 100;
 
                     if (vibrator.hasVibrator()) {
-                        // Define the pattern for continuous vibration (e.g., on-off-on-off pattern)
-                        long[] pattern = {0, vibrationDuration};
-
                         // Create a waveform vibration effect
-                        VibrationEffect effect = VibrationEffect.createWaveform(pattern, vibrationAmplitude);
+                        VibrationEffect effect = VibrationEffect.createOneShot(vibrationDuration, vibrationAmplitude);
                         vibrator.vibrate(effect);
                     }
 
                     // Generate and play tone
                     double frequency = mapValueToFrequency(Math.abs(wifiStateExtra));
-                    double volume = mapValueToVolume(calculateAudioVolume(Math.abs(wifiStateExtra)));
+                    double volume = 0.04;
 
                     short[] buffer = new short[bufferSize];
                     for (int i = 0; i < bufferSize; i++) {
@@ -201,40 +198,16 @@ public class MainActivity extends AppCompatActivity {
     private int mapValueToFrequency(int value) {
         // Map the value to a frequency between 20 and 60, with 20 having the lowest frequency and 60 having the highest
         // Use a higher power of the normalized value to increase sensitivity
-        double normalizedValue = Math.min(1, Math.max(0, 1.0 - (double) (value - 20) / (70 - 20))); // Invert the normalization and normalize the value between 0 and 1
+        double normalizedValue = Math.min(1, Math.max(0, 1.0 - (double) (value - 20) / (80 - 20))); // Invert the normalization and normalize the value between 0 and 1
         // Adjust the scaling factor to make changes in value more noticeable in the frequency
-        int frequencyRange = 5000 - 20; // Define the frequency range
+        int frequencyRange = 5000 - 100; // Define the frequency range
         return (int) (20 + Math.pow(normalizedValue, 4) * frequencyRange); // Map the normalized value to the frequency range
     }
 
-    private double mapValueToVolume(int value) {
-        // Map signal strength to volume level (0.0-1.0)
-        double volume = Math.min(100.0, Math.max(0.0, (double) value / 100.0)); // Assuming value is a percentage (0-100)
-        return volume;
-    }
-
-    private int calculateAudioVolume(int value) {
-        // Map value to volume level
-        // Normalize the value between 0 and 1
-        double normalizedValue = (double) Math.min(1, Math.max(0, 1 - (value - 20) / (70 - 20)));
-        // Define the volume range (0-100)
-        int volumeRange = 50;
-        // Map the normalized value to the volume range
-        return (int) (normalizedValue * volumeRange);
-    }
-
-    private long calculateVibrationDuration(int value) {
-        // Map value to vibration duration
-        long normalizedValue = (long) Math.min(1, Math.max(0, (value - 20) / (70 - 20))); // Normalize the value between 0 and 1
-        // Vibrate for 1 second to 0.1 second based on value
-        int frequencyRange = 1000 - 10;
-        return (long) 10 + (long) Math.pow(normalizedValue, 4) * frequencyRange;
-    }
-
     private int calculateVibrationAmplitude(int value) {
-        double normalizedValue = (double) Math.min(1, Math.max(0, (value - 20) / (70 - 20))); // Normalize the value between 0 and 1
-        int frequencyRange = 1;
-        int vibrationAmplitude = (int) (1 - Math.pow(normalizedValue, 4) * frequencyRange); // Calculate vibration amplitude
+        double normalizedValue = (double) Math.min(1, Math.max(0, (value - 20) / (80 - 20))); // Normalize the value between 0 and 1
+        int frequencyRange = 255 - 1;
+        int vibrationAmplitude = (int) (255 - Math.pow(normalizedValue, 4) * frequencyRange); // Calculate vibration amplitude
         Log.d("VibrationAmplitude", "Amplitude calculated: " + vibrationAmplitude); // Log the result
         return vibrationAmplitude;
     }
